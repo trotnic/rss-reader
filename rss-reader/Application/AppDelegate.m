9befe084ab6240320beca25bb88940b7a7e4c4b2
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-12-07 15:19:14 +0300

    refactor: delete IoC container

--- rss-reader/Application/AppDelegate.m
+++ rss-reader/Application/AppDelegate.m
@@ -9,50 +9,28 @@
 #import "FeedXMLParser.h"
 #import "FeedProvider.h"
 #import "FeedPresenter.h"
-#import "DIContainer.h"
 #import "FeedViewController.h"
 
 @interface AppDelegate ()
-
-@property (nonatomic, retain) DIContainer *container;
 
 @end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [self configureDependencies];
-    self.window.rootViewController =[[[UINavigationController alloc] initWithRootViewController:[self.container resolveServiceOfType:NSStringFromClass(FeedViewController.class)]] autorelease];
+    FeedXMLParser *parser = [FeedXMLParser new];
+    FeedProvider *dataProvider = [[FeedProvider alloc] initWithParser:[parser autorelease]];
+    FeedPresenter *presenter = [[FeedPresenter alloc] initWithProvider:[dataProvider autorelease]];
+    FeedViewController *controller = [[FeedViewController alloc] initWithPresenter:[presenter autorelease]];
+    
+    self.window.rootViewController = [[[UINavigationController alloc] initWithRootViewController:controller] autorelease];
     [self.window makeKeyAndVisible];
+    
+    [controller release];
     return YES;
 }
 
-// MARK: -
-
-- (void)configureDependencies {
-    [self.container registerServiceOfType:NSStringFromClass(FeedXMLParser.class)
-                           withCompletion:^id (id<DIContainerType> container) {
-        return [FeedXMLParser new];
-    }];
-    [self.container registerServiceOfType:NSStringFromClass(FeedProvider.class) withCompletion:^id (id<DIContainerType> container) {
-        return [[FeedProvider alloc] initWithParser:[container resolveServiceOfType:NSStringFromClass(FeedXMLParser.class)]];
-    }];
-    [self.container registerServiceOfType:NSStringFromClass(FeedPresenter.class) withCompletion:^id (id<DIContainerType> container) {
-        return [[FeedPresenter alloc] initWithProvider:[container resolveServiceOfType:NSStringFromClass(FeedProvider.class)]];
-    }];
-    [self.container registerServiceOfType:NSStringFromClass(FeedViewController.class) withCompletion:^id (id<DIContainerType> container) {
-        return [[FeedViewController alloc] initWithPresenter:[container resolveServiceOfType:NSStringFromClass(FeedPresenter.class)]];
-    }];
-}
-
 // MARK: Lazy
-
-- (DIContainer *)container {
-    if(!_container) {
-        _container = [DIContainer new];
-    }
-    return _container;
-}
 
 - (UIWindow *)window {
     if(!_window) {
@@ -66,7 +44,6 @@
 - (void)dealloc
 {
     [_window release];
-    [_container release];
     [super dealloc];
 }
 
